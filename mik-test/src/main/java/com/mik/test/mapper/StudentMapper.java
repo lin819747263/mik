fd13fe5709f5f75b2416aaf501c5325b4b788860
lin819747263 <819747263@qq.com>
2022-05-18 20:56:46 +0800

    demo模块增加

---
+++ mik-test/src/main/java/com/mik/test/mapper/StudentMapper.java
@@ -0,0 +1,8 @@
+package com.mik.test.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mik.test.model.Student;
+
+public interface StudentMapper extends BaseMapper<Student> {
+
+}
