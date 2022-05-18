8b5d834de05d872c2bf8d2801bb47ca635e990b6
lin819747263 <819747263@qq.com>
2022-05-18 22:31:42 +0800

    安全模块完善

--- mik-common/mik-swagger-starter/src/main/java/com/mik/swagger/SwaggerAutoConfiguration.java
+++ mik-common/mik-swagger-starter/src/main/java/com/mik/swagger/SwaggerAutoConfiguration.java
@@ -9,12 +9,16 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @Configuration
 @EnableConfigurationProperties(SwaggerProperties.class)
@@ -39,7 +43,12 @@ public class SwaggerAutoConfiguration {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(properties.getBasePackage()))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .globalOperationParameters(new ArrayList<>(){{
+                    add(new ParameterBuilder().name("Authorization").description("user token")
+                            .modelRef(new ModelRef("string")).parameterType("header")
+                            .required(false).build());
+                }});
     }
 
     private ApiInfo apiInfo(SwaggerProperties properties) {
