2e3743d295d063fd2e2549fb47f5b4b74171dd93
lin819747263 <819747263@qq.com>
2022-05-22 22:50:56 +0800

    短信登录完善

--- mik-common/mik-security/src/main/java/com/mik/security/provider/UserNameAndPwdProvider.java
+++ mik-common/mik-security/src/main/java/com/mik/security/provider/UserNameAndPwdProvider.java
@@ -3,11 +3,11 @@ package com.mik.security.provider;
 import com.mik.security.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -26,11 +26,11 @@ public class UserNameAndPwdProvider implements AuthenticationProvider {
         UserDetails user = userService.loadUserByUsername((String)token.getPrincipal());
 
         if(user == null){
-            throw new UsernameNotFoundException("用户不存在");
+            throw new AuthenticationServiceException("用户不存在");
         }
 
         if(!passwordEncoder.matches((String) token.getCredentials(), user.getPassword())){
-            throw new UsernameNotFoundException("用户名或者密码错误");
+            throw new AuthenticationServiceException("用户名或者密码错误");
         }
 
         UsernamePasswordAuthenticationToken authenticationResult = new UsernamePasswordAuthenticationToken(user,user.getAuthorities());
