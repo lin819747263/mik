ecef6137a137e27ef7358e595f614a4f22022c58
lin819747263 <819747263@qq.com>
2022-06-15 19:16:14 +0800

    完善文档，优化项目

--- mik-nav/src/main/java/com/mik/nav/mapper/StudentMapper.java
+++
@@ -1,8 +0,0 @@
-package com.mik.nav.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.mik.nav.model.Student;
-
-public interface StudentMapper extends BaseMapper<Student> {
-
-}
